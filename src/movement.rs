0f9cdcb266c99266fdf6897ea379c93f74a1264c
earlsab <62688314+earlsab@users.noreply.github.com>
2025-01-04 19:04:17 +0800

    remove update rotation

--- src/movement.rs
+++ src/movement.rs
@@ -22,9 +22,14 @@ fn update_position(mut query: Query<(&Velocity, &mut Transform)>, time: Res<Time
     }
 }
 
-fn update_rotation(mut query: Query<(&Velocity, &mut Transform)>, time: Res<Time>) {
-    for (velocity, mut transform) in query.iter_mut() {
-        // FIXME: Allow for rotation via input from angvel
-        transform.rotate_y(velocity.angvel.y * time.delta_secs())
-    }
-}
+// fn update_rotation(mut query: Query<(&Velocity, &mut Transform)>, time: Res<Time>) {
+//     for (velocity, mut transform) in query.iter_mut() {
+//         // FIXME: Allow for rotation via input from angvel
+//         // transform.rotation.y = velocity.angvel.y * time.delta_secs()
+//         // transform.rotate_y( * time.delta_secs())
+//         // transform.rotate_local_y(velocity.angvel.y * time.delta_secs());
+//         // transform.rotate_y(velocity.angvel.y);
+//         let rotate_by: Quat = Quat::from_xyzw(0.0, velocity.angvel.y, 0.0, 1.0);
+//         // transform.rotate(rotate_by);
+//     }
+// }
